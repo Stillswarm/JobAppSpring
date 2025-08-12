98c7f0279017f6c879b442c5af857c8dc5bf7e2b
Stillswarm <abhi555sri@gmail.com>
2025-08-12 11:04:22 +0530

    create controller, service, repository for Jobs

---
+++ src/main/java/org/example/jobapp/job/Job.java
@@ -0,0 +1,77 @@
+package org.example.jobapp.job;
+
+import jakarta.persistence.*;
+
+@Entity
+//@Table(name = "job_table")
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String desc;
+    private String maxSalary;
+    private String minSalary;
+    private String location;
+
+    public Job(Long id, String title, String desc, String maxSalary, String minSalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.maxSalary = maxSalary;
+        this.minSalary = minSalary;
+        this.location = location;
+    }
+
+    public Job() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
